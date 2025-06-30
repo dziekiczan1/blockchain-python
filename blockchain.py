fb1e4a7ce644db3d987820ee45940435b482e86a
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-06-30 19:44:44 +0200

    add save and load data methods

--- blockchain.py
+++ blockchain.py
@@ -18,11 +18,29 @@ open_transactions = []
 owner = 'Peter'
 participants = {'Peter'}
 
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = file_content[0]
+        open_transactions = file_content[1]
+
+load_data()
+
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(str(blockchain))
+        f.write('\n')
+        f.write(str(open_transactions))
+
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
     guess_hash = hash_string_256(guess)
     return guess_hash[0:2] == '00'
+
 
 def proof_of_work():
     last_block = blockchain[-1]
@@ -79,6 +97,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -102,6 +121,7 @@ def mine_block():
         'proof': proof
     }
     blockchain.append(block)
+    save_data()
     return True
 
 
