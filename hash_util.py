1297fb58564051c838340cefc9c0dd1d672934f3
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-07-08 20:14:51 +0200

    fix has_block function

--- hash_util.py
+++ hash_util.py
@@ -1,11 +1,20 @@
-import hashlib
+import hashlib as hl
 import json
 
-
 def hash_string_256(string):
-    """ Create a SHA-256 hash of a string. """
-    return hashlib.sha256(string.encode()).hexdigest()
+    """Create a SHA256 hash for a given input string.
+
+    Arguments:
+        :string: The string which should be hashed.
+    """
+    return hl.sha256(string).hexdigest()
 
 
 def hash_block(block):
-    return hash_string_256(json.dumps(block, sort_keys=True).encode())
+    """Hashes a block and returns a string representation of it.
+
+    Arguments:
+        :block: The block that should be hashed.
+    """
+    hashable_block = block.__dict__.copy()
+    return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
